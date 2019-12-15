330f914e60c25f9c3fafeba70b7b44a24574af1c
唐成 <cheng.tang@gongbangbang.com>
2019-12-15 19:27:10 +0800

    todo mergeTwoLists

--- src/main/java/com/hymxys/leetcode/problems/MergeTwoSortedListsSolution.java
+++ src/main/java/com/hymxys/leetcode/problems/MergeTwoSortedListsSolution.java
@@ -10,7 +10,22 @@ import com.hymxys.leetcode.bo.ListNodeBO;
  */
 public class MergeTwoSortedListsSolution {
 
-
+    /**
+     * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+     * <p>
+     * 示例：
+     * <p>
+     * 输入：1->2->4, 1->3->4
+     * 输出：1->1->2->3->4->4
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
     public ListNodeBO mergeTwoLists(ListNodeBO l1, ListNodeBO l2) {
         return null;
     }
